467c617633c9c4fd521a3979045315d13b34e9ac
Илья <96050258+paladinxb@users.noreply.github.com>
2023-02-25 13:04:33 +1000

    Update main.py

--- memegenerator/main.py
+++ memegenerator/main.py
@@ -6,28 +6,35 @@ from PIL import Image, ImageDraw, ImageFont
 font = ImageFont.truetype('arial.ttf', random.randint(40, 100))
 
 image_dir = ('C:/Users/79842/memegenerator/memes')
-
 image_files = os.listdir(image_dir)
 
-image_file = random.choice(image_files)
+words = ["BIG W", "BIG L", "LOL", "ROFL", "WTF", "If u know u know", "Let it be", "OMG", "ASAP BABE"]
 
-image = Image.open(os.path.join(image_dir, image_file))
+def create_meme():
+    image_file = random.choice(image_files)
+    image = Image.open(os.path.join(image_dir, image_file))
+    height = image.height
+    width = image.width
+    draw = ImageDraw.Draw(image)
+    sentence = random.choice(words)
+    x = randint(300, 350)
+    y = randint(300, 350)
+    draw.text((x, y), sentence, fill=1280128, font=font)
+    return image
 
-width, height = image.size
-
-draw = ImageDraw.Draw(image)
-
-words = ["BIG W", "BIG L", "LOL", "ROFL"]
-
-sentence = random.choice(words)
-
-
-#text_width, text_height = draw.textlength(words, font)
-#x = (width - text_width) / 2
-#y = height - text_height - 50
-x = randint(200, 600)
-y = randint(200, 700)
-
-draw.text((x, y), sentence, fill=1280128, font=font)
-
-image.save('meme.png')
+while True: 
+    # запрос на продолжение работы
+    answer = input("Введите 'Продолжить' для создания файла или 'Остановить' для выхода: ")
+    
+    # если пользователь вводит 'Остановить', завершаем цикл
+    if answer == "Остановить":
+        break
+    elif answer == "Продолжить":
+        meme_name = input("Введите имя файла: ")
+        file_extension = ".png"
+        png_filename = f"{meme_name}{file_extension}"
+        meme_image = create_meme()
+        meme_image.save(png_filename)
+        #meme_image.show()
+    else:
+        print("Неверный ввод. Пожалуйста, введите 'Продолжить' или 'Остановить'.")
